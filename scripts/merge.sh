71a1e3fb3eba2670fe78673de7619989c9acad7e
skykiseki <evelinesdd@qq.com>
2023-10-28 11:18:20 +0800

    add merge-scripts

---
+++ scripts/merge.sh
@@ -0,0 +1,5 @@
+Python merge_llama_with_chinese_lora_low_mem.py \
+    --base_model path/to/base_model \
+    --lora_model path/to/lora_model \
+    --output_type huggingface \
+    --output_dir path/to/output_dir
